7e6b59c9c29537bc97a7725701d59e7514570fc0
杨攀 <yangpan@baixing.com>
2016-10-31 19:27:08 +0800

    mobx react

--- sourceCode/dest/index/index.js
+++ sourceCode/dest/index/index.js
@@ -1,23 +1,8 @@
 webpackJsonp([0,1],[
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var _test = __webpack_require__(1);
-	
-	console.log(_test.b);
-
-/***/ },
-/* 1 */
 /***/ function(module, exports) {
 
 	"use strict";
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var b = exports.b = 21;
 
 /***/ }
 ]);
